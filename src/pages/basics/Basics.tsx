ec25e858517fadf11e8254e709ebb7b28f120ca4
Asif Kassam Ali <asif.k.dev@gmail.com>
2022-09-15 18:03:43 +0200

    small fix

--- src/pages/basics/Basics.tsx
+++ src/pages/basics/Basics.tsx
@@ -3,7 +3,7 @@ import TypeScriptImg_1 from "../../assets/images/Typescript_01.png"
 import TypeScriptImg_2 from "../../assets/images/Typescript_02.png"
 import TypeScriptImg_3 from "../../assets/images/Typescript_03.png"
 import TypeScriptImg_4 from "../../assets/images/Typescript_04.png"
-
+import TypeScriptImg_5 from "../../assets/images/Typescript_05.png"
 
 const Basics = () => {
     const [styleState] = useState({
@@ -11,7 +11,10 @@ const Basics = () => {
         marginLeft: "10px",
         border: "2px solid #073d94"
     })
-    const [imageArrayOne] = useState([TypeScriptImg_1, TypeScriptImg_2, TypeScriptImg_3, TypeScriptImg_4])
+
+    const [imageArrayOne] = useState([TypeScriptImg_1, TypeScriptImg_2, TypeScriptImg_3, TypeScriptImg_4, TypeScriptImg_5]);
+    const [age, setAge] = useState(Number)
+
     return (
         <div>
             <h2> Basics </h2>
@@ -21,6 +24,9 @@ const Basics = () => {
                         <img key={key} src={image} alt="typescript vs javascript" width="500px" style={styleState} />
                     ))
                 }
+                <h5>Assign Age</h5> 
+                <input  onChange={(e) => setAge(parseInt(e.target.value))} />
+                {age && <p> You are {age} years old </p>}
             </div>
         </div>
     )
